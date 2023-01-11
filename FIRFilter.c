57ca8d8b4bc01369a0fe3a35b1f62fc865cf5393
Tbthanh <thanhtb2003ltv@gmail.com>
2023-01-11 23:30:48 +0700

    more problem so solve huray
    just include 2 more library, one for i2c to use the dac pcf8591 and the other one for utilizing the adc onboard for the thermometer. I love my life so much ughhhhhhhh

--- FIRFilter.c
+++ FIRFilter.c
@@ -1,14 +1,72 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "FIRFilter.h"
+#include "hardware/i2c.h"
 
-static float FIR_IMPULSE_RESPONSE=[FIR_FILTER_LENGTH] = {}; //array contain impulse response
 
-void FIRFilter_Init(FIRFilter *fir) {
+static float FIR_IMPULSE_RESPONSE[FIR_FILTER_LENGTH] = {}; //array contain impulse response
+
+void FIRFilter_Init(FIRFilter *fir) { /*this function is just to clear stuff*/
+
+    /*Clear filter buffer, that circle buffer noted in header*/
+    for (uint8_t n = 0;  n< FIR_FILTER_LENGTH; n++){
+
+        fir->buf[n] = 0.0f; //Havent understand this yet :(
+        
+    }
+
+    /*Reset buffer index*/
+    fir->bufIndex = 0;
+    
+    /*clear filter output*/
+    fir->out = 0.0f;
 
 }
+
+float FIRFilter_Update(FIRFilter *fir, float inp)   {/*the actual hard as fire stuff*/
+
+    /*Store latest sample in buffer*/
+    fir->buf[fir->bufIndex] = inp;
+
+    /*increment buffer index and wrap around if necessary*/
+    fir->bufIndex++;
+
+    if (fir->bufIndex == FIR_FILTER_LENGTH) {
+        
+        fir->bufIndex = 0;
+
+    }
+
+    /*compute new output sample (via convolution )*/
+    fir->out = 0.0f;
+
+    uint8_t sumIndex = fir->bufIndex;
+
+    for (uint8_t n = 0; n < FIR_FILTER_LENGTH; n++) {
+
+        /*decrement index and wrap if necessary*/
+        if (sumIndex > 0) {
+            
+            sumIndex--;
+
+        } else{
+
+            sumIndex = FIR_FILTER_LENGTH - 1;
+
+        }
+
+        /*multiply impulse response with shifted input sample and add to output*/
+        fir->out += FIR_IMPULSE_RESPONSE[n] * fir->buf[sumIndex];
+
+    }
+
+    /*return filtered output*/
+    return fir->out;
+}
+
 int main() {
 
+    /*
     const uint led_pin = 25;
 
     // Initialize LED pin
@@ -28,4 +86,8 @@ int main() {
         gpio_put(led_pin, false);
         sleep_ms(1000);
     }
+    */
+
+
+    
 }
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # FIR_with_rasp_pico
  Create Fir filter with raspberry pi pico and C/C++ and python programing language
+
+some guide material that I use:
+
+FIR Filter Design and Software Implementation - Phil's Lab #17  (main source of FIRFilter.c)
+https://youtu.be/uNNNj9AZisM
+
+Beginners Guide to I2C on the Raspberry Pi Pico (BNO055 IMU Example) -by Learn Embedded System
+https://youtu.be/092xFEmAS98
+
+How to Set Up a Project in Visual Studio Code for the Pi Pico - Blink LED -by Learn Embedded System
+https://youtu.be/Q1Kfg8k54jM
+
+How to Use the Temperature Sensor on the Raspberry Pi Pico in C -by Learn Embedded System
+https://youtu.be/dsu-c6kSIe0
+
+Raspberry Pi Pico and RP2040 - C/C++ Part 1: Blink and VS Code
+https://www.digikey.com/en/maker/projects/raspberry-pi-pico-and-rp2040-cc-part-1-blink-and-vs-code/7102fb8bca95452e9df6150f39ae8422
+
+Raspberry Pi Pico (RP2040) I2C Example with MicroPython and C/C++
+https://www.digikey.com/en/maker/projects/raspberry-pi-pico-rp2040-i2c-example-with-micropython-and-cc/47d0c922b79342779cdbd4b37b7eb7e2
+
